6902bc7364d6aff6b620dab23ea2c3dfdaa0419d
VinnyVerbose <vinny@bootcampredemption.com>
2026-06-03 13:19:24 -0400

    added logic flow to review questions when quiz is finished

--- main.js
+++ main.js
@@ -6,7 +6,9 @@ const state = {
     screen: "welcome",
     currentQuestionIndex: 17,
     answerHistory: [],
-    quizMode: "review"
+    quizMode: "review",
+    reviewFilter: null,
+    isReviewing: false
 }
 
 init();
@@ -16,6 +18,7 @@ function init(){
 }
 
 function render(){
+    console.log(state)
     switch(state.screen){
         case "welcome":
             renderWelcome();
@@ -25,6 +28,7 @@ function render(){
         case "question":
             renderQuestion();
             addAnswerEvents();
+            addNextButtonEvent();
             break;
 
         case "thankYou":
@@ -33,9 +37,13 @@ function render(){
 
         case "results":
             renderResults();
+            addReviewButtonEvents();
             break;
     }
 }
+
+
+// Renders ===========================================
 
 function renderWelcome(){
     let html = 
@@ -61,6 +69,9 @@ function renderQuestion(){
             ${q.options.map((option, index) => {
                 return renderAnswer(option, index);
             }).join('')}
+        </div>
+        <div id="btnNextWrapper">
+            ${state.isReviewing ? '<button id="btnNext">Next</button>' : ''}
         </div>
     </div>
     `
@@ -104,6 +115,10 @@ function renderResults(){
             <p>Results By Category:</p>
             ${categoryHtml}
             
+            <div id="reviewOptionsWrapper">
+                <button id="btnReviewAll">Review All Questions</button>
+                <button id="btnReviewIncorrect">Review Incorrect Questions</button>
+            </div>
         </div>
     `
     app.innerHTML = html;
@@ -113,14 +128,76 @@ function renderCategoryData(category){
     return `<p>${category.name} : <span>${category.correctCount} / ${category.totalAnswered}</span></p>`
 }
 
+
+// Add Events===================================
+
+function addStartEvent(){
+    document.getElementById('btnStart').addEventListener('click', () => {
+        state.screen = "question";
+        render();
+    });
+}
+
+function addAnswerEvents(){
+    let answers = document.getElementsByClassName('answer');
+
+    for(let i = 0; i < answers.length; i++){
+        answers[i].addEventListener('click', ()=> {
+            handleAnswerSelected(answers[i].dataset.answerIndex);
+        });
+    }
+}
+
+function addReviewButtonEvents(){
+    document.getElementById("btnReviewAll").addEventListener('click', () => {
+        state.isReviewing = true;
+        state.reviewFilter = "all";
+        state.screen = "question";
+        state.currentQuestionIndex = 0;
+        render();
+    });
+
+    document.getElementById("btnReviewIncorrect").addEventListener('click', () => {
+        state.reviewFilter = "incorrect";
+    });
+}
+
+function addNextButtonEvent(){
+    if(state.isReviewing){
+        document.getElementById("btnNext").addEventListener('click', () => {
+            console.log('clicked next')
+            advanceReview();
+        })
+    }
+}
+
+// Business Logic===============================
+
+function handleAnswerSelected(index){
+    if(!state.isReviewing){
+        recordAnswer(index);
+        if(state.quizMode === "normal" || state.quizMode === "review"){
+            advanceQuiz();
+        }
+    }
+}
+
+function recordAnswer(index){
+    let isCorrect = data.questions[state.currentQuestionIndex].correctAnswer === Number(index);
+    state.answerHistory.push({
+        questionId: data.questions[state.currentQuestionIndex].id,
+        selectedAnswerIndex: Number(index),
+        isCorrect
+    });
+}
+
 function getQuizResults(){
     let totalAnswered = state.answerHistory.length;
     let totalCorrect = 0;
     let results = {categories: []};
 
     state.answerHistory.forEach(answer => {
-        let questionIndex = answer.questionId - 1;
-        let question = data.questions[questionIndex];
+        let question = data.questions.find(question => question.id === answer.questionId);
         let category = question.category;
 
         if(answer.isCorrect) totalCorrect++;
@@ -150,50 +227,17 @@ function getQuizResults(){
     return results;
 }
 
-// Add Events
-function addStartEvent(){
-    document.getElementById('btnStart').addEventListener('click', () => {
-        state.screen = "question";
-        render();
-    });
-}
-
-function addAnswerEvents(){
-    let answers = document.getElementsByClassName('answer');
-
-    for(let i = 0; i < answers.length; i++){
-        answers[i].addEventListener('click', ()=> {
-            handleAnswerSelected(answers[i].dataset.answerIndex);
-        });
-    }
-}
-
-function handleAnswerSelected(index){
-    recordAnswer(index);
-    if(state.quizMode === "normal" || state.quizMode === "review"){
-        advanceQuiz();
-    }
-}
-
-function recordAnswer(index){
-    let isCorrect = data.questions[state.currentQuestionIndex].correctAnswer === Number(index);
-    state.answerHistory.push({
-        questionId: data.questions[state.currentQuestionIndex].id,
-        selectedAnswerIndex: Number(index),
-        isCorrect
-    });
-}
-
 function advanceQuiz(){
     if(state.currentQuestionIndex >= data.questions.length - 1){
+        state.reviewFilter = null;
         switch(state.quizMode){
             
-            case "normal":
-                state.screen = "thankYou";
-                break;
-
             case "review":
                 state.screen = "results";
+                break;
+
+            default:
+                state.screen = "thankYou";
                 break;
         }
         
@@ -201,4 +245,15 @@ function advanceQuiz(){
         state.currentQuestionIndex++;
     }
     render();
+}
+
+function advanceReview(){
+    if(state.currentQuestionIndex >= data.questions.length - 1){
+        state.reviewFilter = null;
+        state.screen = "results";
+        state.isReviewing = false;
+    }else{
+        state.currentQuestionIndex++;
+    }
+    render();    
 }
